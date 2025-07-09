9c9b280cd5f6d29cc212337855120cc1e8a74734
jmttoki <jmttoki@users.noreply.github.com>
2025-07-09 09:29:19 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -29,7 +29,7 @@
               </el-form-item>
               <el-form-item label="后端地址:">
                 <el-select v-model="form.customBackend" allow-create filterable @change="selectChanged"
-                  placeholder="可输入自己的后端" style="width: 100%">
+                  placeholder="https://dvzselixrpso.ap-southeast-1.clawcloudrun.com" style="width: 100%">
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
